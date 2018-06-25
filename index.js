0cb21357b527f3f613b9e4e16042406543ca82ae
Paola Neira <paola1792@gmail.com>
2018-06-25 19:26:52 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,20 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100)
+  var itemObj = {itemName: item, price: price};
+  cart.push(itemObj);
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
 }
 
 function total() {
-  // write your code here
 }
 
 function removeFromCart(item) {
-  // write your code here
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
 }

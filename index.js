1c2b9994427552aed94b1f219b15e88a19570860
Paola Neira <paola1792@gmail.com>
2018-06-26 14:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,21 +26,19 @@ function addToCart(item) {
   return `${item} has been added to your cart.`
 }
 
-function viewCart() {
+function viewCart(cart) {
   const line = "In your cart, you have "
-  var itemObj2 = {[item]:price}
-  var cart= cart.push(itemObj2)
   if (cart.length === 0) {
     return "Your shopping cart is empty"
   }
-  else if (cart2.length === 1) {
-    return line + Object.keys(cart2) + " at $" + Object.values(cart2) + "."
+  else if (cart.length === 1) {
+    return line + Object.keys(cart) + " at $" + Object.values(cart) + "."
   }
   else {
-  const line3 = ", and " + Object.keys(cart2[cart2.length-1]) + "at $" + Object.keys(cart2[cart2.length-1]) + "."
-  for (let i=0; i<cart2.length-1; i++) {
+  const line3 = ", and " + Object.keys(cart[cart.length-1]) + "at $" + Object.keys(cart[cart.length-1]) + "."
+  for (let i=0; i<cart.length-1; i++) {
     var line2 = []
-    var line2 =  Object.keys(cart2[i]) + " at $" + Object.values(cart2[i])
+    var line2 =  Object.keys(cart[i]) + " at $" + Object.values(cart[i])
     }
     return line + line2 + line3
   }

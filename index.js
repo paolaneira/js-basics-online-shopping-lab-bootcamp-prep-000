9f626a2d60eee88b74025e9c7e9295ca55f8e831
Paola Neira <paola1792@gmail.com>
2018-06-26 14:02:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+<<<<<<< HEAD
   var price = Math.floor(Math.random() * 100)
   var itemObj = {itemName: item, price: price};
   cart.push(itemObj);
   return `${item} has been added to your cart.`
 }
+=======
+  var price = Math.floor(Math.random())
+  cart = {"item": price}
+>>>>>>> 5c3e969fe857cf1b271ae546ab027b4b6ec7b765
 
 function viewCart() {
 }

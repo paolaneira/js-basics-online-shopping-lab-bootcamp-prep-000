2ea1dce0733fac1e848624bd91fdaaa4b740a5d6
Paola Neira <paola1792@gmail.com>
2018-06-26 14:12:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,18 +10,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-<<<<<<< HEAD
   var price = Math.floor(Math.random() * 100)
-  var itemObj = {itemName: item, price: price};
+  var itemObj = {"itemName": [item], "itemPrice": price};
   cart.push(itemObj);
   return `${item} has been added to your cart.`
 }
-=======
-  var price = Math.floor(Math.random())
-  cart = {"item": price}
->>>>>>> 5c3e969fe857cf1b271ae546ab027b4b6ec7b765
 
 function viewCart() {
+  const line = "In your cart, you have "
+  var itemObj2 = {[item]:price}
+  var cart2= cart.push(itemObj2)
+  If (cart2.length === 0) {
+    console.log("Your shopping cart is empty")
+  }
+  else if (cart2.length === 1) {
+    return line + Object.keys(cart2) + " at $" + Object.values(cart2) + "."
+  }
+  else {
+  const line3 = ", and " + Object.keys(cart2[cart2.length-1]) + "at $" + Object.keys(cart2[cart2.length-1]) + "."
+  for (let i=0; i<cart2.length-1; i++) {
+    var line2[i] =  Object.keys(cart2[i]) + " at $" + Object.values(cart2[i])
+    }
+    return line + line2 + line3
+  } 
 }
 
 function total() {
